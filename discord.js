dd8e56c0322b0456b746589df777d551e59b3215
0xNim <57798165+Nim1com@users.noreply.github.com>
2023-06-09 16:38:24 -0400

    done:)

--- discord.js
+++ discord.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, MessageEmbed } = require('discord.js');
 const axios = require('axios');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -11,35 +11,18 @@ client.on('ready', () => {
 });
 
 client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand() || interaction.commandName !== 'top-mods') {
+  if (!interaction.isCommand()) {
     return;
   }
 
-  const response = await axios.get('https://mstats.astromods.xyz/top-mods');
-  const data = response.data;
-
-  const sortedByTotal = data.mods.sort((a, b) => b.totalCount - a.totalCount);
-  const sortedByEnabled = data.mods.sort((a, b) => b.enabledCount - a.enabledCount);
-
-  const embed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Top Mods')
-    .addFields(
-      { name: 'Sorted by Total', value: sortedByTotal.map((mod) => `${mod.mod}: ${mod.totalCount}`).join('\n') },
-      { name: 'Sorted by Enabled', value: sortedByEnabled.map((mod) => `${mod.mod}: ${mod.enabledCount}`).join('\n') },
-    );
-
-  const reply = await interaction.reply({ embeds: [embed], fetchReply: true });
-  const message = await reply.fetch();
-
-  const interval = setInterval(async () => {
+  if (interaction.commandName === 'top-mods') {
     const response = await axios.get('https://mstats.astromods.xyz/top-mods');
     const data = response.data;
 
     const sortedByTotal = data.mods.sort((a, b) => b.totalCount - a.totalCount);
     const sortedByEnabled = data.mods.sort((a, b) => b.enabledCount - a.enabledCount);
 
-    const updatedEmbed = new Discord.MessageEmbed()
+    const embed = new MessageEmbed()
       .setColor('#0099ff')
       .setTitle('Top Mods')
       .addFields(
@@ -47,15 +30,36 @@ client.on('interactionCreate', async (interaction) => {
         { name: 'Sorted by Enabled', value: sortedByEnabled.map((mod) => `${mod.mod}: ${mod.enabledCount}`).join('\n') },
       );
 
-    await message.edit({ embeds: [updatedEmbed] });
-  }, 60000);
+    await interaction.reply({ embeds: [embed], fetchReply: true });
 
-  interaction.client.once('interactionCreate', (interaction) => {
-    if (interaction.isCommand() && interaction.commandName === 'stop-top-mods') {
-      clearInterval(interval);
-      interaction.reply('Stopped updating top mods.');
-    }
-  });
+    const interval = setInterval(async () => {
+      const response = await axios.get('https://mstats.astromods.xyz/top-mods');
+      const data = response.data;
+
+      const sortedByTotal = data.mods.sort((a, b) => b.totalCount - a.totalCount);
+      const sortedByEnabled = data.mods.sort((a, b) => b.enabledCount - a.enabledCount);
+
+      const updatedEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Top Mods')
+        .addFields(
+          { name: 'Sorted by Total', value: sortedByTotal.map((mod) => `${mod.mod}: ${mod.totalCount}`).join('\n') },
+          { name: 'Sorted by Enabled', value: sortedByEnabled.map((mod) => `${mod.mod}: ${mod.enabledCount}`).join('\n') },
+        );
+
+      await interaction.editReply({ embeds: [updatedEmbed] });
+    }, 60000);
+
+    interaction.client.once('interactionCreate', (interaction) => {
+      if (interaction.isCommand() && interaction.commandName === 'stop-top-mods') {
+        clearInterval(interval);
+        interaction.reply('Stopped updating top mods.');
+      }
+    });
+  } else if (interaction.commandName === 'stop-top-mods') {
+    clearInterval(interval);
+    await interaction.reply('Stopped updating top mods.');
+  }
 });
 
 client.login(token);
